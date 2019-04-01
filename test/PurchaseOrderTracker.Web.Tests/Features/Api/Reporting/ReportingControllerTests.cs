9490cf646c0648a757ad2f37360218c272dc2390
Kieren <kierendixon1986@gmail.com>
2019-04-01 12:44:30 +1100

    fix: test broken from swagger changes

--- test/PurchaseOrderTracker.Web.Tests/Features/Api/Reporting/ReportingControllerTests.cs
+++ test/PurchaseOrderTracker.Web.Tests/Features/Api/Reporting/ReportingControllerTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using PurchaseOrderTracker.Web.Features.Api.Reporting;
@@ -16,16 +15,14 @@ namespace PurchaseOrderTracker.Web.Tests.Features.Api.Reporting
             [Test]
             public async Task AnyResult_IsReturnedAsObjectResult()
             {
-                var result = new ShipmentsSummary.Result(1,2,3,4);
+                var result = new ShipmentsSummary.Result(1, 2, 3, 4);
                 var mediator = new Mock<IMediator>();
                 mediator.Setup(m => m.Send(It.IsAny<ShipmentsSummary.Query>(), It.IsAny<CancellationToken>())).ReturnsAsync(result);
-                ShipmentsSummary.Query query = new ShipmentsSummary.Query();
 
-                var actualResult = await new ReportingController(mediator.Object).ShipmentsSummary(query);
-                var objectResult = (ObjectResult) actualResult;
+                var actualResult = await new ReportingController(mediator.Object).ShipmentsSummary();
 
                 mediator.Verify(m => m.Send(It.IsAny<ShipmentsSummary.Query>(), It.IsAny<CancellationToken>()), Times.Once());
-                Assert.That(result, Is.SameAs(objectResult.Value));
+                Assert.That(result, Is.SameAs(actualResult.Value));
             }
         }
     }
