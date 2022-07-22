e586c9bd6244516632f8b9c096e748720ebca132
Kieren Dixon <kierendixon1986@gmail.com>
2022-07-22 19:38:33 +1000

    delete old docker script

--- docker-start.ps1
+++
@@ -1,9 +0,0 @@
-
-# build all images
-docker build --tag purchase-order-tracker-webapi -f src/PurchaseOrderTracker.WebApi/Dockerfile .
-docker build --tag purchase-order-tracker-angular -f src/PurchaseOrderTracker.WebUI.Angular/Dockerfile .
-docker build --tag purchase-order-tracker-admin -f src/PurchaseOrderTracker.WebUI.Admin/Dockerfile .
-docker build --tag purchase-order-tracker-identity -f src/PurchaseOrderTracker.Identity/Dockerfile .
-
-# start the application
-docker-compose up
